4146e4955810cebfa4caba8dffa062fcd54545bb
satoshishimazaki <sattori06010601@gmail.com>
2016-06-06 01:36:18 +0900

    delete analytics

--- wp-content/themes/grider_tcd015_nowhere/header.php
+++ wp-content/themes/grider_tcd015_nowhere/header.php
@@ -147,17 +147,6 @@ body {
 }
 </style>
 
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-71011506-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
-
 <script type="text/javascript">jQuery(function() {  
     jQuery("a").click(function(e) {        
         var ahref = jQuery(this).attr('href');
